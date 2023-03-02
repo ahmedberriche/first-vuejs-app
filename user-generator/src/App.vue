54304ae14ccc211f891a4b45030fa9a1bc006a38
ahmedberriche <ahmedberriche1993@gmail.com>
2023-03-02 21:31:32 +0100

    first commit

---
+++ user-generator/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Header title="myFirstApp" />
+  <div class="container">
+    <Card />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Card from "./components/Card.vue";
+export default {
+  name: "App",
+  components: {
+    Header,
+    Card,
+  },
+};
+</script>
+<style scoped>
+.container {
+  max-width: 80%;
+  margin: 0 auto;
+}
+</style>
---
+++ user-generator/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="main-content">
+        <span>{{ title }}</span>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  name: "Header",
+  props: ["title"],
+};
+</script>
+<style scoped>
+.header {
+  background: #439cfa;
+  height: 60px;
+  width: 100%;
+  color: #fff;
+  font-size: 24px;
+}
+.container {
+  /* max-width: 600px; */
+  max-width: 80%;
+  margin: 0 auto;
+  height: 100%;
+}
+.main-content {
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+</style>
